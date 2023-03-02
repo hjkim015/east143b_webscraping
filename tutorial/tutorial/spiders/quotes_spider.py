6f7b109082bdbd99eb9ce5d0057c8d03896883dc
Hannah Kim <47065568+hjkim015@users.noreply.github.com>
2023-03-02 22:36:32 +0000

    finished the tutorial

---
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+
+import scrapy
+
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+
+    def start_requests(self):
+        urls = [
+            'https://quotes.toscrape.com/page/1/',
+            'https://quotes.toscrape.com/page/2/',
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        '''
+        Save all of the web information and html into a file
+        '''
+        # page = response.url.split("/")[-2]
+        # filename = f'quotes-{page}.html'
+        # Path(filename).write_bytes(response.body)
+        # self.log(f'Saved file {filename}')
+
+        #Gather all of the relevant information. 
+        for quote in response.css('div.quote'):
+            yield {
+                'text': quote.css('span.text::text').get(),
+                'author': quote.css('small.author::text').get(),
+                'tags': quote.css('div.tags a.tag::text').getall(),
+            }
+
+
+
